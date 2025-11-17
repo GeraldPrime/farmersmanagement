3d6e255062498966a6424dc0aaa782c998dc820f
admin <admin@admins-MacBook-Pro.local>
2025-11-17 02:08:16 +0100

    commit4

--- farmersmanagement/settings.py
+++ farmersmanagement/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
